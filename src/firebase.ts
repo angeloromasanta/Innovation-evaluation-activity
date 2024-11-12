4a0ad12f1dde14e9a001aadab1e3b1152356588a
Angelo Romasanta <angelokenneth.romasanta@esade.edu>
2024-11-12 12:14:13 +0100

    Firebase update

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,15 +4,15 @@ import { getFirestore, collection, doc, setDoc, onSnapshot } from 'firebase/fire
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Only initialize Firebase if it hasn't been initialized already
+// Initialize Firebase
 let app;
 let db;
 let auth;
@@ -21,10 +21,8 @@ try {
   app = initializeApp(firebaseConfig);
   db = getFirestore(app);
   auth = getAuth(app);
-} catch (error) {
-  if (!/already exists/.test(error.message)) {
-    console.error('Firebase initialization error', error.stack);
-  }
+} catch (error: any) {
+  console.error('Firebase initialization error', error.stack);
 }
 
 export { app, db, auth };
